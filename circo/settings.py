140b198b1857dcca6454a688a55c6a0e803b7788
PreshiDev <pgift1059@gmail.com>
2026-04-21 01:26:16 +0100

    updated or added some libraries

--- circo/settings.py
+++ circo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hrv^$52-zv-6$&e#l8qouh&e++lh%d@=vevxaon8m%z-q348y-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# WhiteNoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'circo.urls'
 
