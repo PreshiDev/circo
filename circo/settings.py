6bcc51d276ce23b8a204977df0e9f8c9d31e4adc
PreshiDev <pgift1059@gmail.com>
2026-04-21 01:12:17 +0100

    third commit made

--- circo/settings.py
+++ circo/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'circo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dpg-d7jbsohkh4rs73fjih30-a',
-#         'USER': 'circo_user',
-#         'PASSWORD': 'hpozV7AfgGVWWQ3OdZKjPZrDJt8plv0J',
-#         'HOST': 'postgresql://circo_user:hpozV7AfgGVWWQ3OdZKjPZrDJt8plv0J@dpg-d7jbsohkh4rs73fjih30-a/circo',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dpg-d7jbsohkh4rs73fjih30-a',
+        'USER': 'circo_user',
+        'PASSWORD': 'hpozV7AfgGVWWQ3OdZKjPZrDJt8plv0J',
+        'HOST': 'postgresql://circo_user:hpozV7AfgGVWWQ3OdZKjPZrDJt8plv0J@dpg-d7jbsohkh4rs73fjih30-a/circo',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
