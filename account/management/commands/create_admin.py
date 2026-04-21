0aef955be0d08fded3c861e93676d6036f9a8c07
PreshiDev <pgift1059@gmail.com>
2026-04-21 02:12:44 +0100

    added automatic admin creation

--- account/management/commands/create_admin.py
+++ account/management/commands/create_admin.py
@@ -1,22 +1,28 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+
 class Command(BaseCommand):
     help = "Create superuser automatically"
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
 
-        username = "admin"
         email = "admin@gmail.com"
         password = "admin123"
+        full_name = "Admin User"
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
-            self.stdout.write(self.style.SUCCESS("Superuser created"))
-        else:
-            self.stdout.write("Superuser already exists")
+        try:
+            # 🔥 FIX: use email instead of username
+            if not User.objects.filter(email=email).exists():
+                User.objects.create_superuser(
+                    email=email,
+                    password=password,
+                    full_name=full_name
+                )
+                self.stdout.write(self.style.SUCCESS("Superuser created"))
+            else:
+                self.stdout.write("Superuser already exists")
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error: {e}"))
